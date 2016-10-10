174ede699ed7cf7c0c8aefcc8ecb86b3277b92e4
Angel Chen <rikugou@gmail.com>
2016-10-10 15:45:31 -0400

    Add faker and generate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  user = User.create!( username: Faker::Internet.user_name, email: Faker::Internet.email, password: "password")
+end
