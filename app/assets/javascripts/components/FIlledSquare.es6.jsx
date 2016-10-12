e0622ed7fef1b12283bb9804695bf38788f30d29
Angel Chen & Jenn Su <pair+angel-chen.jennysoup@devbootcamp.com>
2016-10-12 10:00:44 -0400

    Add svg code and build card components

--- app/assets/javascripts/components/FIlledSquare.es6.jsx
+++ app/assets/javascripts/components/FIlledSquare.es6.jsx
@@ -1,7 +1,9 @@
 class FilledSquare extends React.Component {
   render() {
+    let { color } = this.props;
     return(
-      Hello
+      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 100 100" xmlSpace="preserve"><g>
+      <path d="M99,99H1V1h98V99z" fill={color}/></g></svg>
     );
   }
 }
